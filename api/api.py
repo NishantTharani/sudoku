54115d9472d180511826a281f3fd6c0ee563ba08
user <user@pop-os.localdomain>
2021-07-05 17:46:30 +0100

    Basic styled sudoku grid

--- api/api.py
+++ api/api.py
@@ -2,7 +2,7 @@
 # Description: API for generating/playing/solving a sudoku game
 
 
-from flask import Flask
+from flask import Flask, jsonify
 import time
 import sudoku
 
@@ -11,3 +11,12 @@ app = Flask(__name__)
 
 @app.route('/api/new_puzzle', methods=['GET'])
 def get_new_puzzle():
+    # return sudoku.generate_new_puzzle()
+    grid = [[0] * 9] * 9
+    out = {'grid': grid, 'n': 3}
+    return jsonify(out)
+
+
+@app.route('/api/time', methods=['GET'])
+def get_time():
+    return "100"
--- src/App.js
+++ src/App.js
@@ -1,33 +1,64 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+function Grid(props) {
+  let height = props.n ** 2;
+  let n = props.n;
+  let out = [];
+
+  for (let row = 0; row < height; row++) {
+    let rowList = []
+    for (let col = 0; col < height; col++) {
+      let i = (row * height) + col;
+      let isTop = row === 0;
+      let isBottom = row === height - 1;
+      let isLeft = col === 0;
+      let isRight = col === height - 1;
+      let isMiniTop = !isTop && row % n === 0;
+      let isMiniBottom = !isBottom && (row + 1) % n === 0;
+      let isMiniLeft = !isLeft && col % n === 0;
+      let isMiniRight = !isRight && (col + 1) % n === 0;
+      rowList.push(<div key={i.toString()} className={`cell ${isTop ? "top" : ""} ${isBottom ? "bottom" : ""} ${isLeft ? "left" : ""} ${isRight ? "right" : ""}
+                                                ${isMiniTop ? "miniTop" : ""} ${isMiniBottom ? "miniBottom" : ""}
+                                                ${isMiniLeft ? "miniLeft" : ""} ${isMiniRight ? "miniRight" : ""}`}> {props.grid[row][col]} </div> )
+    }
+    out.push(<div key={row.toString()} className={'row'}>{rowList}</div> )
+  }
+
+  for (let i = 0; i < height ** 2; i++) {
+
+
+      }
+
+  return (
+      <div className={'grid'}>{out}</div>
+  );
+}
+
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
+  const [grid, setGrid] = useState([]);
+  const [n, setN] = useState(0);
+
+  function _newGame() {
+    fetch('/api/new_puzzle')
+        .then(response => response.text())
+        .then(data => JSON.parse(data))
+        .then(newGame => {
+          console.log(newGame.grid);
+          setGrid(newGame.grid);
+          setN(newGame.n);
+        });
+
+  }
 
   useEffect(() => {
-    fetch('/api/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
+    _newGame();
   }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current time is {currentTime}</p>
-      </header>
+      <Grid grid={grid} n={n}/>
     </div>
   );
 }
